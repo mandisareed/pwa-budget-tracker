7fed27d9388f160e128d618be50a4b8baea95d65
Mandisa Reed <mandisa.reed@gmail.com>
2020-08-05 15:58:16 -0700

    added service worker code to activate and fetch

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,96 @@
+//print a message to the console upon registering
+console.log("Hi from your service worker!");
+
+// create variable for the cache name
+const STATIC_CACHE = "static-cache-v1";
+const DATA_CACHE = "data-cache";
+
+//create an array containing the orutes to cache
 const FILES_TO_CACHE = [
     "/",
     "/db.js",
+    "/index.js",
     "/index.html",
-    "/manifest.json",
     "/manifest.webmanifest",
-    "/topic.html",
-    "/style.css",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   ];
   
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
   
-  self.addEventListener("install", event => {
+  
+  //install
+  // when the sw installs
+  // then add all routes to cache for static assets
+  // then take over (skip waiting)
+  self.addEventListener("install", function (event) {
+    // then add all routes to cache for static assets
     event.waitUntil(
-      caches.open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+      caches.open(STATIC_CACHE).then(function (cache) {
+        console.log("Success! Your files were pre-cached!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+    // immediately take over (skip waiting)
+    self.skipWaiting();
   });
   
   // The activate handler takes care of cleaning up old caches.
-  self.addEventListener("activate", event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  // when the sw activates
+  // remove any outdated caches
+  self.addEventListener("activate", function (event) {
+    //const currentCaches = [STATIC_CACHE, DATA_CACHE];
     event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-      }).then(cachesToDelete => {
-        return Promise.all(cachesToDelete.map(cacheToDelete => {
-          return caches.delete(cacheToDelete);
-        }));
-      }).then(() => self.clients.claim())
-    );
+      caches.keys().then((keyList) => {
+        return Promise.all(
+        keyList.map((key) => {
+          if (key !== STATIC_CACHE && key !== DATA_CACHE) {
+            console.log("Old cache data being removed", key);
+          return caches.delete(key);
+        }}));
+      })
+    );self.clients.claim();
   });
   
-  self.addEventListener("fetch", event => {
-    if (event.request.url.startsWith(self.location.origin)) {
+
+  // whenever the client triggers fetch
+  // respond from cache falling back to the network
+  self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
       event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-  
-          return caches.open(RUNTIME_CACHE).then(cache => {
+        caches.open(DATA_CACHE).then((cache) => {
+          // if (cachedResponse) {
+          //   return cachedResponse;
+          // }
             return fetch(event.request).then(response => {
-              return cache.put(event.request, response.clone()).then(() => {
+              if (response.status === 200) {
+               cache.put(event.request.url, response.clone());
+              }
                 return response;
+              })
+              //if request to the network is not successful, fetch data from cache
+              .catch((err) => {
+                return cache.match(event.request);
               });
-            });
-          });
-        })
-      );
-    }
-  });
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+          );
+          return;
+        }
+        // checked the cache, then do a network request
+        // this way, data stored offline will be available
+        event.respondWith(
+          caches.open(STATIC_CACHE)
+          .then((cache) => {
+            return cache.match(event.request)
+            .then(function (response) {
+              return response || fetch(event.request);
+            })
+          }
+        ))});
